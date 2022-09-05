99c8aa26f9931f3d9b1c94711edbeb884988b7b1
Swaroop Akkineni <swa7@pitt.edu>
2022-09-05 10:19:37 -0400

    chapter 2 is done

--- src/main/kotlin/Chapter2.kt
+++ src/main/kotlin/Chapter2.kt
@@ -21,6 +21,47 @@ fun main() {
     println("-------------------------")
     exercise2point2()
     println("-------------------------")
+    exercise2point3()
+    println("-------------------------")
+}
+
+fun exercise2point5() {
+    /**
+     *  Implement the higher-order function that composes two functions.
+     */
+//    compose()
+}
+
+fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C = {
+    a: A -> f(g(a))
+}
+
+fun exercise2point4() {
+    /**
+     *   Implement uncurry, which reverses the transformation of curry. Note that
+     *   since -> associates to the right, (A) -> ((B) -> C)
+     *   can be written as (A) -> (B) -> C.
+     */
+//    uncurry()
+}
+
+fun <A, B, C> uncurry(f: (A) -> (B) -> C): (A, B) -> C = {
+    a: A, b: B -> f(a)(b)
+}
+
+fun exercise2point3() {
+    /**
+     *   Let’s look at another example, currying, which converts
+     *   a function f of two arguments into a function
+     *   of one argument that partially applies f.
+     *   Here again there’s only one implementation
+     *   that compiles. Write this implementation.
+     */
+//    curry()
+}
+
+fun <A, B, C> curry(f: (A, B) -> C): (A) -> (B) -> C = {
+    a: A -> { b -> f(a, b) }
 }
 
 fun exercise2point2() {
@@ -35,8 +76,8 @@ fun exercise2point2() {
      *    refer to the Extension methods and properties sidebar.
      */
 
-    var sort = fun(a: Int, b: Int ): Boolean { return a < b }
-
+//    var sort = fun(a: Int, b: Int ): Boolean { return a < b }
+    var sort = { a: Int, b: Int -> a < b }
     println(isSorted(listOf(0, 1, 2, 3, 4), sort))
     println(isSorted(listOf(1, 2), sort))
     println(isSorted(listOf(2), sort))
